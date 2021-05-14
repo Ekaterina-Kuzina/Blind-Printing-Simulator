0413606490b70371b89361d60082ef3f07a0983c
Ekaterina <katya.cuzina2011@yandex.ru>
2021-05-14 21:21:19 +0300

    moving of green highlit letter is done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const body = document.querySelector('body');
 const content = 'В жизни все имеет свое начало, так и любая наука и искусство берут свое начало где-то в глубине веков, а далее развиваются, совершенствуются, образуются новые направления, новые течения. Это относится и к фотографии, которую я воспринимаю как искусство, развитие которого напрямую связано с наукой, имею ввиду развитие фототехники.  В этой статье под названием «История фотографии кратко» собраны самые главные факты о зарождении и развитии великого искусства фотографии.';
 const textLettersArr = content.split('');
 
-function createSpan(){
+function createSpan() {
     textLettersArr.forEach(item => {
         const span = document.createElement("span");
         span.innerHTML = `${item}`;
@@ -12,24 +12,31 @@ function createSpan(){
 }
 createSpan();
 
-const spans = document.querySelectorAll('span');
-
-function moveSquare(i) {
-    spans.forEach(() =>{
+function highlightLetter(i) {
+    const spans = document.querySelectorAll('span');
+    spans.forEach(() => {
         spans[i].classList.add('green');
     });
 }
+function removeHighlightLetter() {
+    const spans = document.querySelectorAll('span');
+    spans.forEach((span) => {
+        span.classList.remove('green');
+    });
+}
+
 const userLettersArr = [];
 const result = [];
 let i = 0;
 body.addEventListener('keydown', (e) => {
+    removeHighlightLetter();
     function compareLetters() {
         if (textLettersArr[i] == e.key) {
             console.log(textLettersArr[i]);
             userLettersArr.push(e.key);
             result.push(textLettersArr[i]);
-            console.log('good')
-            moveSquare(i);
+            console.log('good');
+            highlightLetter(i);
             i++;
         } else {
             console.log('bad');

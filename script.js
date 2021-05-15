4ac31b2beab723f9b77c85d75fb6e9cd792caaf6
Ekaterina <katya.cuzina2011@yandex.ru>
2021-05-15 22:38:17 +0300

    interactive by highlight is added

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+const btn = document.querySelector('button');
 const wrapper = document.querySelector('.wrapper');
 const body = document.querySelector('body');
 const content = 'В жизни все имеет свое начало, так и любая наука и искусство берут свое начало где-то в глубине веков, а далее развиваются, совершенствуются, образуются новые направления, новые течения. Это относится и к фотографии, которую я воспринимаю как искусство, развитие которого напрямую связано с наукой, имею ввиду развитие фототехники.  В этой статье под названием «История фотографии кратко» собраны самые главные факты о зарождении и развитии великого искусства фотографии.';
 const textLettersArr = content.split('');
+const userLettersArr = [];
+const result = [];
+let indexOfElement = 0;
 
 function createSpan() {
     textLettersArr.forEach(item => {
@@ -12,42 +16,47 @@ function createSpan() {
 }
 createSpan();
 
-function highlightLetter(i) {
-    const spans = document.querySelectorAll('span');
+const spans = document.querySelectorAll('span');
+function highlightLetter(indexOfElement, styleOfLetter) {
     spans.forEach(() => {
-        spans[i].classList.add('green');
-    });
-}
-function removeHighlightLetter() {
-    const spans = document.querySelectorAll('span');
-    spans.forEach((span) => {
-        span.classList.remove('green');
+        spans[indexOfElement].classList.add(styleOfLetter);
     });
 }
 
-const userLettersArr = [];
-const result = [];
-let i = 0;
-body.addEventListener('keydown', (e) => {
-    removeHighlightLetter();
-    function compareLetters() {
-        if (textLettersArr[i] == e.key) {
-            console.log(textLettersArr[i]);
-            userLettersArr.push(e.key);
-            result.push(textLettersArr[i]);
-            console.log('good');
-            highlightLetter(i);
-            i++;
-        } else {
-            console.log('bad');
-        }
+function removeHighlightLetter(styleOfLetter) {
+    spans.forEach((span) => {
+        span.classList.remove(styleOfLetter);
+    });
+}
+function compareLetters(e) {
+    if (textLettersArr[indexOfElement] == e.key) {
+        console.log(textLettersArr[indexOfElement]);
+        userLettersArr.push(e.key);
+        result.push(textLettersArr[indexOfElement]);
+        console.log('good');
+        indexOfElement++;
+        highlightLetter(indexOfElement, 'green');
+    } else {
+        highlightLetter(indexOfElement, 'red');
+        console.log('bad');
     }
-    compareLetters();
+}
+btn.addEventListener('click', () => {
+    highlightLetter(indexOfElement, 'green');
 });
+
+body.addEventListener('keydown', (e) => {
+    removeHighlightLetter('green');
+    removeHighlightLetter('red');
+    compareLetters(e);
+});
+
+
+
 console.log(userLettersArr);
 console.log(textLettersArr)
-console.log(i);
-console.log(result)
+console.log(indexOfElement);
+console.log(result);
 
 
 

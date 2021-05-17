4b20290ed69aa995db98552ad8fd66c858399034
Ekaterina <katya.cuzina2011@yandex.ru>
2021-05-17 10:08:33 +0300

    Work with server(by fetch) and testing interface are done

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
-const btn = document.querySelector('button');
-const wrapper = document.querySelector('.wrapper');
+
+const startPage = document.querySelector('.start-page');
+const startBtn = document.querySelector('.start-page__btn');
+const wrapperContent = document.querySelector('.wrapper-content');
 const body = document.querySelector('body');
-const content = 'В жизни все имеет свое начало, так и любая наука и искусство берут свое начало где-то в глубине веков, а далее развиваются, совершенствуются, образуются новые направления, новые течения. Это относится и к фотографии, которую я воспринимаю как искусство, развитие которого напрямую связано с наукой, имею ввиду развитие фототехники.  В этой статье под названием «История фотографии кратко» собраны самые главные факты о зарождении и развитии великого искусства фотографии.';
-const textLettersArr = content.split('');
+let textLettersArr;
 const userLettersArr = [];
 const result = [];
 let indexOfElement = 0;
+let spans;
 
-function createSpan() {
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+//server
+function getData(url) {
+    fetch(url)
+        .then(data => data.json())
+        .then(result => {
+            console.log(result)
+            textLettersArr = result[getRandomInt(result.length)].text.split('');
+            createSpan(textLettersArr);
+            spans = document.querySelectorAll('span');
+        });
+}
+getData('http://localhost:3000/content');
+//
+function createSpan(textLettersArr) {
     textLettersArr.forEach(item => {
         const span = document.createElement("span");
         span.innerHTML = `${item}`;
-        wrapper.appendChild(span);
+        wrapperContent.appendChild(span);
     });
 }
-createSpan();
-
-const spans = document.querySelectorAll('span');
 function highlightLetter(indexOfElement, styleOfLetter) {
-    spans.forEach(() => {
-        spans[indexOfElement].classList.add(styleOfLetter);
-    });
+    spans[indexOfElement].classList.add(styleOfLetter);
 }
-
 function removeHighlightLetter(styleOfLetter) {
     spans.forEach((span) => {
         span.classList.remove(styleOfLetter);
     });
 }
+function greenLetter(){
+    spans[indexOfElement-1].classList.add('greenLetter');
+    console.log('xyi')
+}
 function compareLetters(e) {
+    console.log(textLettersArr)
     if (textLettersArr[indexOfElement] == e.key) {
         console.log(textLettersArr[indexOfElement]);
         userLettersArr.push(e.key);
@@ -41,7 +58,15 @@ function compareLetters(e) {
         console.log('bad');
     }
 }
-btn.addEventListener('click', () => {
+function addClassHide(block) {
+    block.classList.add('hide');
+}
+function removeClassHide(block) {
+    block.classList.remove('hide');
+}
+startBtn.addEventListener('click', () => {
+    addClassHide(startPage);
+    removeClassHide(wrapperContent);
     highlightLetter(indexOfElement, 'green');
 });
 
@@ -49,12 +74,11 @@ body.addEventListener('keydown', (e) => {
     removeHighlightLetter('green');
     removeHighlightLetter('red');
     compareLetters(e);
+    greenLetter();
 });
 
-
-
 console.log(userLettersArr);
-console.log(textLettersArr)
+// console.log(textLettersArr)
 console.log(indexOfElement);
 console.log(result);
 

a14ddb2213954e258dfab9369ca84bdea09d984f
Ekaterina <katya.cuzina2011@yandex.ru>
2021-05-18 09:29:44 +0300

    js and sass are updated

--- js/content/countAccuracy.js
+++ js/content/countAccuracy.js
@@ -1,4 +1,5 @@
 export let accuracy = 100;
+
 export function countAccuracy() {
     accuracy = (accuracy - 0.1).toFixed(1);
     document.querySelector('#certificateAccuracy').innerHTML = accuracy;
